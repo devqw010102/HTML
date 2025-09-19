814c5ee2bfd2b2ec00c40efbba893bbec0f704f9
devqw010102 <qw010102@gmail.com>
2025-09-19 11:25:00 +0900

    Add files via upload

---
+++ chap16/js/ex10.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+while(true) {
+    count++;
+    let oddEven = prompt(`홀 / 짝 중 선택`);
+
+    if(oddEven == "짝") {
+        break;
+    }
+}
+
+console.log(count);
---
+++ chap16/js/ex9.js
@@ -0,0 +1,15 @@
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br>");
+// document.writeln("안녕하세요 <br><br><br>");
+
+
+for(let i = 0; i < 3; i++ ){
+    document.writeln("안녕하세요 <br>");
+}
