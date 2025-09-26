0e1181979f3d3e7e2c3eb5aef6a04459da662137
devqw010102 <qw010102@gmail.com>
2025-09-26 16:46:19 +0900

    Add files via upload

---
+++ Task/js_paper test/task.js
@@ -0,0 +1,57 @@
+// Q1
+// console.log("Q1");
+// console.log("true, false")
+
+// Q2
+// console.log("Q2");
+// console.log("1");
+
+// Q3
+// console.log("Q3");
+// let number = parseInt(prompt("Q3)Number"));
+// if(number % 2 == 0) 
+//     console.log("짝수");
+// else
+//     console.log("홀수");
+
+// Q4
+// console.log("Q4");
+// for(let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+// Q5
+// console.log("Q5");
+// let x = prompt("X");
+// let y = prompt("Y");
+// console.log(multiply(x, y));
+
+// function multiply(x, y) {
+//     return x * y;
+// }
+
+// Q6
+// console.log("Q6");
+// let arr = ["사과", "배", "바나나"];
+// console.log("배");
+
+// Q7
+// console.log("Q7");
+// let student = {name : "홍길동", age : 20};
+// console.log(student.name);
+
+// Q8
+// console.log("Q8");
+// let nums = [1, 2, 3, 4];
+// let result = nums.filter(n => n % 2 === 0);
+// console.log("2, 4");
+
+// Q9
+// console.log("Q9");
+// let msg = document.querySelector("#msg");
+// msg.textContent = "Hi";
+
+// Q10
+// console.log("Q10");
+// let btn = document.querySelector("#btn");
+// btn.addEventListener('click', () => console.log('버튼이 클릭되었습니다.'));
